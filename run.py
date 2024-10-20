67fa07ffc65548151ed97eb61c2f3279b5b5853e
Ibrahim Abedghane <abedghaneibrahim94@gmail.com>
2024-10-20 17:06:35 +0000

    Added the functionality to display the game grid

--- run.py
+++ run.py
@@ -2,4 +2,15 @@ import random
 
 def create_grid(size):
     return [['~' for _ in range(size)] for _ in range(size)]
-    
+
+def display_grid(grid, hide_ships=True):
+    size = len(grid)
+    print(" " + " ".join(stri(i) for i in range(size)))
+    for idx, row in enumerate(grid):
+        display_row = []
+        for cell in row:
+            if hide_ships and cell == 'S':
+                display_row.append('~')
+            else:
+                display_row.append(cell)
+        print(f"{idx} "+" ".join(display_row))
