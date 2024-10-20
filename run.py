92411a3461ca000dff41525ced2b9496f366b45b
Ibrahim Abedghane <abedghaneibrahim94@gmail.com>
2024-10-20 17:13:29 +0000

    added functionality to randomly add ships to the grid

--- run.py
+++ run.py
@@ -14,3 +14,24 @@ def display_grid(grid, hide_ships=True):
             else:
                 display_row.append(cell)
         print(f"{idx} "+" ".join(display_row))
+
+def place_ships(grid, num_ships):
+    siz = len(grid)
+    ships_placed = 0
+    while ships_placed < num_ships:
+        orientation = random.choice(['H', 'V'])
+        ship_length = random.randint(2, 3) #Ships of length 2 or 3
+        if orientation == 'H':
+            row = random.randint(0, size - 1)
+            col = random.randint(0, size - ship_length)
+            if all(grid[row][col + i] == '~' for i in range(ship_length)):
+                for i in range(ship_length):
+                    grid[row][col + i] = 'S'
+                ships_placed += 1
+        else:
+            row = random.randint(0, size - ship_length)
+            col = random.randint(0, size - 1)
+            if all(grid[row + i][col] == '~' for i in range(ship_length)):
+                for i in range(ship_length):
+                    grid[row + i][col] = 'S'
+                ships_placed += 1
